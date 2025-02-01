b9b5fa9c07278eb569becd07595ea07e84b42ec6
Yevhenii <117980195+Anahiell@users.noreply.github.com>
2025-02-01 12:23:50 +0200

    Basic start
    Add gitignore, add back and frontend

---
+++ backend/AirbnbAPI/AirbnbAPI/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirbnbAPI.Controllers
+{
+    [Route("api/hello")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetHello()
+        {
+            return Ok(new { message = "Привет из BACKEND .NET API!" });
+        }
+    }
+}
