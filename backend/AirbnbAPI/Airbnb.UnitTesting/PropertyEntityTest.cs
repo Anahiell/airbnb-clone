4e06e42dcdad6744dc4b022fabcb5fc5a898c00b
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2025-02-02 02:11:28 +0200

    [ref] Github Actions файл для тестирования при использования Pipeline с тестами

---
+++ backend/AirbnbAPI/Airbnb.UnitTesting/PropertyEntityTest.cs
@@ -0,0 +1,15 @@
+﻿using Airbnb.Infrastructure.Entities;
+using Airbnb.UnitTesting.Generators;
+
+namespace Airbnb.UnitTesting;
+
+public class PropertyEntityTest
+{
+    [Theory]
+    [ClassData(typeof(PropertyEntityGenerator))]
+    public void Test1(PropertyEntity property)
+    {
+        Assert.NotNull(property);
+        Assert.True(property.Id > 0);
+    }
+}
