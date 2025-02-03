6bbe7aabce5581a12495fec3205f74a8da9600e4
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2025-02-03 20:23:42 +0200

    [feat] Добавил файлы миграции

---
+++ backend/AirbnbAPI/Airbnb.Infrastructure/Migrations/20250201221146_AddSeedDataToPropertyEntity.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Airbnb.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedDataToPropertyEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PropertyEntity",
+                column: "Id",
+                values: new object[]
+                {
+                    1,
+                    2,
+                    3
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PropertyEntity",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PropertyEntity",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PropertyEntity",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
