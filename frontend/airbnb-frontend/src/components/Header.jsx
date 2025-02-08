3fe5360552de20b4179ec36b89f570f66da4e435
iriwamaked <iriwamaked@gmail.com>
2025-02-08 17:52:04 +0200

    Header styling row1

--- frontend/airbnb-frontend/src/components/Header.jsx
+++ frontend/airbnb-frontend/src/components/Header.jsx
@@ -6,33 +6,50 @@ const Header = ({ onOpenModal, user, onLogout, onToggleMap }) => {
 
   return (
     <header className={styles.header}>
-      {/* Логотип */}
-      <Link to="/" className={styles.logo}>HomeFU</Link>
+      <div className={styles.row + " " + styles["top-row"]}>
+        {/* Логотип */}
+        <Link to="/" className={styles.logo}>HomeFU</Link>
 
-      {/* Поисковая строка */}
-      <div className={styles.searchBar}>
-        <input type="text" placeholder="Поиск помещения" />
-        <button>🔍</button>
+        <nav className={styles.nav}>
+          <a href="#">Варіанти помешкань</a>
+          <a href="#">Враження</a>
+          <a href="#">Онлайн-враження</a>
+        </nav>
+
+        {/* Кнопки справа */}
+        <div className={styles.rightButtons}>
+          <Link to="/listings" className={styles.offerBtn}>Запропонувати помешкання на HomeFU</Link>
+          {/* Кнопка "Показать на карте" видна только на странице listing */}
+          {location.pathname === "/listings" && (
+            <button className={styles.mapButton} onClick={onToggleMap}>📍 Показать на карте</button>
+          )}
+
+          {user ? (
+            <div className={styles.userMenu}>
+              <span>Привет, {user.username}</span>
+              <button className={styles.logoutBtn} onClick={onLogout}>Выйти</button>
+            </div>
+          ) : (
+            // <button className={styles.profileIcon} onClick={onOpenModal}>👤</button>
+            <button className={styles.ovalButton} onClick={onOpenModal}>
+              <span></span>
+              <div className={styles.personContainer}>
+                <div class={styles.personHead}></div>
+                <div class={styles.personBody}></div>
+              </div>
+            </button>
+          )}
+        </div>
+      </div>
+      <div className={styles.row + " " + styles["bottom-row"]}>
+        {/* Поисковая строка */}
+        <div className={styles.searchBar}>
+          <input type="text" placeholder="Поиск помещения" />
+          <button>🔍</button>
+        </div>
+
       </div>
 
-      {/* Кнопки справа */}
-      <div className={styles.rightButtons}>
-        <Link to="/listings" className={styles.offerBtn}>Запропонувати помещение</Link>
-
-        {/* Кнопка "Показать на карте" видна только на странице listing */}
-        {location.pathname === "/listings" && (
-          <button className={styles.mapButton} onClick={onToggleMap}>📍 Показать на карте</button>
-        )}
-
-        {user ? (
-          <div className={styles.userMenu}>
-            <span>Привет, {user.username}</span>
-            <button className={styles.logoutBtn} onClick={onLogout}>Выйти</button>
-          </div>
-        ) : (
-          <button className={styles.profileIcon} onClick={onOpenModal}>👤</button>
-        )}
-      </div>
     </header>
   );
 };
