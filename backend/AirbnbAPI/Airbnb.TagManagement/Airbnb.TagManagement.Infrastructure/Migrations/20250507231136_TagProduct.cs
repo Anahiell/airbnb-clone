087965e84d40b88ed77e43f6d547278480e798d5
Waken Vincent <111260358+VotreWaken@users.noreply.github.com>
2025-05-08 19:11:20 +0300

    [ref] Миграция

---
+++ backend/AirbnbAPI/Airbnb.TagManagement/Airbnb.TagManagement.Infrastructure/Migrations/20250507231136_TagProduct.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Airbnb.TagsManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class TagProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductTag",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    TagId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductTag", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductTag");
+        }
+    }
+}
